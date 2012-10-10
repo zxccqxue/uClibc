f806c2868e5e59597881a7b735cca3785ea1d304
Markos Chandras <markos.chandras@imgtec.com>
2012-10-10 16:06:57 +0100

    utimes: Use utimensat if arch does not have the utimes syscall
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- libc/sysdeps/linux/common/utimes.c
+++ libc/sysdeps/linux/common/utimes.c
@@ -8,11 +8,30 @@
  */
 
 #include <sys/syscall.h>
-#include <utime.h>
 #include <sys/time.h>
+#include <fcntl.h>
+#include <utime.h>
+#include <stddef.h>
 
 
-#ifdef __NR_utimes
+#if defined(__NR_utimensat) && !defined(__NR_utimes)
+int utimes(const char *file, const struct timeval tvp[2])
+{
+	struct timespec ts[2], *times;
+	if (tvp) {
+		times = ts;
+		times[0].tv_sec = tvp[0].tv_sec;
+		times[0].tv_nsec = tvp[0].tv_usec * 1000;
+		times[1].tv_sec = tvp[1].tv_sec;
+		times[1].tv_nsec = tvp[1].tv_usec * 1000;
+	} else {
+		times = NULL;
+	}
+
+	return utimensat(AT_FDCWD, file, times, 0);
+}
+
+#elif defined(__NR_utimes)
 _syscall2(int, utimes, const char *, file, const struct timeval *, tvp)
 #else
 #include <stdlib.h>
