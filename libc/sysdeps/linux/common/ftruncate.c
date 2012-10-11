dfeefe2462e49aef5a981f9bce2b1a7b74b98734
Markos Chandras <markos.chandras@imgtec.com>
2012-10-11 11:08:28 +0100

    ftruncate: Use ftruncate64 if arch does not have the ftruncate syscall
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- libc/sysdeps/linux/common/ftruncate.c
+++ libc/sysdeps/linux/common/ftruncate.c
@@ -8,8 +8,25 @@
  */
 
 #include <sys/syscall.h>
+#include <stdint.h>
 #include <unistd.h>
 
 
+#if defined(__NR_ftruncate64) && !defined(__NR_ftruncate)
+int ftruncate(int fd, __off_t length)
+{
+#if defined __UCLIBC_HAS_LFS__
+	return ftruncate64(fd, length);
+#elif __WORDSIZE == 32
+	uint32_t high = length & (1<<31) ? 0xffffffff : 0;
+
+	return INLINE_SYSCALL(ftruncate64, 3, fd,
+			      __LONG_LONG_PAIR (high, length));
+#endif
+}
+libc_hidden_def(ftruncate);
+
+#else
 _syscall2(int, ftruncate, int, fd, __off_t, length)
 libc_hidden_def(ftruncate)
+#endif
