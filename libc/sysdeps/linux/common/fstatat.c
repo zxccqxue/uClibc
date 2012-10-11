8ff0eea78e6b7dd6c0b7af79fac9707da641bd69
Markos Chandras <markos.chandras@imgtec.com>
2012-10-11 12:15:50 +0100

    stat: Use fstatat64 if arch does not have the stat syscall
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- libc/sysdeps/linux/common/fstatat.c
+++ libc/sysdeps/linux/common/fstatat.c
@@ -41,6 +41,7 @@ int fstatat(int fd, const char *file, struct stat *buf, int flag)
 	return ret;
 #endif /* __ARCH_HAS_DEPRECATED_SYSCALLS__ */
 }
+libc_hidden_def(fstatat)
 #else
 /* should add emulation with fstat() and /proc/self/fd/ ... */
 #endif
