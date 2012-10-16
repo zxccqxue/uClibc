eb2ab056131304462880a54efe5a2b3d243232b0
Markos Chandras <markos.chandras@imgtec.com>
2012-10-16 10:22:21 +0100

    fstat: Use fstat64() if arch does not have the fstat syscall
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- libc/sysdeps/linux/common/fstat.c
+++ libc/sysdeps/linux/common/fstat.c
@@ -12,6 +12,24 @@
 #include <sys/stat.h>
 #include "xstatconv.h"
 
+#if defined(__NR_fstat64) && !defined(__NR_fstat)
+int fstat(int fd, struct stat *buf)
+{
+	int result = INLINE_SYSCALL(fstat64, 2, fd, buf);
+	if (result == 0) {
+		/* Did we overflow? */
+		if (buf->__pad1 || buf->__pad2 || buf->__pad3
+		    || buf->__pad4 || buf->__pad5
+		    || buf->__pad6 || buf->__pad7) {
+			__set_errno(EOVERFLOW);
+			return -1;
+		}
+	}
+	return result;
+}
+libc_hidden_def(fstat)
+
+#elif defined(__NR_fstat)
 int fstat(int fd, struct stat *buf)
 {
 	int result;
@@ -38,7 +56,9 @@ int fstat(int fd, struct stat *buf)
 }
 libc_hidden_def(fstat)
 
-#if ! defined __NR_fstat64 && defined __UCLIBC_HAS_LFS__
+#if ! defined (__NR_fstat64) && defined __UCLIBC_HAS_LFS__
 strong_alias_untyped(fstat,fstat64)
 libc_hidden_def(fstat64)
+#endif
+
 #endif
--- libc/sysdeps/linux/common/fstat64.c
+++ libc/sysdeps/linux/common/fstat64.c
@@ -21,6 +21,7 @@ static __inline__ _syscall2(int, __syscall_fstat64,
 
 int fstat64(int fd, struct stat64 *buf)
 {
+#ifdef __ARCH_HAS_DEPRECATED_SYSCALLS__
 	int result;
 	struct kernel_stat64 kbuf;
 
@@ -29,6 +30,9 @@ int fstat64(int fd, struct stat64 *buf)
 		__xstat64_conv(&kbuf, buf);
 	}
 	return result;
+#else
+	return __syscall_fstat64(fd, buf);
+#endif
 }
 libc_hidden_def(fstat64)
 #endif
